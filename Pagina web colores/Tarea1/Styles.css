b4403eb33cf93e4e3e4f0598d9245d6fd1d27b08
Yahir56 <161994090+Yahir56@users.noreply.github.com>
2024-04-28 15:55:10 -0600

    Add files via upload

---
+++ Pagina web colores/Tarea1/Styles.css
@@ -0,0 +1,48 @@
+h1 {
+
+    background-color: blue; color: white;
+    font-size: 36px;
+    display: flex;
+    justify-content: center;
+    border-color: gray;
+    border-radius: 2px;
+    border-style: solid;
+    
+}
+
+h2 {
+
+    background-color: rgb(207, 206, 206);
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    font-family: Arial, sans-serif;
+    font-size: 18px;
+    border-color: rgb(64, 63, 63);
+    border-radius: 1px;
+    border-style: solid;
+
+}
+
+ p {
+    
+    color: black;
+
+ }
+
+footer {
+
+    background-color: black;
+    p {
+        color: white;
+    }
+    color: white;
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    font-family: Verdana, sans-serif;
+    font-size: 14px;
+
+}
+ 
+
